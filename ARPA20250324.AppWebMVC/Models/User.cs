fb4ab94d7f467955bdef0fe41dfd74b92b56e5da
andrearocioperez <rocioarriola1204@outlook.com>
2025-03-24 08:11:15 -0600

    Se crearon los controladores con sus vistas, se añadio una vista mas llamada Login a User y se modifico el modelo de User

--- ARPA20250324.AppWebMVC/Models/User.cs
+++ ARPA20250324.AppWebMVC/Models/User.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ARPA20250324.AppWebMVC.Models;
 
 public partial class User
 {
+    [Display(Name = "Id")]
     public int Id { get; set; }
-
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Username { get; set; } = null!;
-
+    [Display(Name = "Email")]
+    [Required(ErrorMessage = "El Email es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El correo electrónico no tiene un formato válido.")]
     public string Email { get; set; } = null!;
-
+    [Display(Name = "Contraseña")]
+    [Required(ErrorMessage = "El password es obligatorio.")]
+    [DataType(DataType.Password)]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El passowrd debe tener entre 5 y 50 caracteres.")]
     public string Password { get; set; } = null!;
-
+    [Display(Name = "Rol")]
     public string Role { get; set; } = null!;
-
+    [Display(Name = "Notas")]
     public string? Notes { get; set; }
+
+    [NotMapped]
+    [StringLength(40, MinimumLength = 5, ErrorMessage = "El password debe tener entre 5 y 50 caracteres.")]
+    [Display(Name = "ConfirmarPassword")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
+    public string? ConfirmarPassword { get; set; } = null!;
 }
